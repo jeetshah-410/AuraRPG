d05940b1d71243cb3b9be811feec9089028d7800
jeetshah-410 <jeetshah0410@gmail.com>
2024-06-12 22:35:41 +0530

    first

---
+++ Source/AuraRPG/AuraRPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AuraRPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AuraRPG, "AuraRPG" );
